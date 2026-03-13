2cc67c8a888f9a44c82afa71972d0ee1c79a9b19
qasemshox-afk <qasem.shox@gmail.com>
2026-03-13 11:27:14 +0200

    Update ForegroundService.java

--- app/src/main/java/com/android/system/protection/ForegroundService.java
+++ app/src/main/java/com/android/system/protection/ForegroundService.java
@@ -3,21 +3,26 @@ package com.android.system.protection;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+
 import com.android.system.protection.utils.NotificationHelper;
 
 public class ForegroundService extends Service {
+
     @Override
     public void onCreate() {
         super.onCreate();
         NotificationHelper.createNotificationChannel(this);
-        startForeground(NotificationHelper.NOTIFICATION_ID, NotificationHelper.getNotification(this));
+        startForeground(NotificationHelper.NOTIFICATION_ID, 
+                NotificationHelper.getForegroundNotification(this));
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        return START_STICKY;
+        return START_STICKY; // يضمن عودة الخدمة للعمل إذا تم إيقافها
     }
 
     @Override
-    public IBinder onBind(Intent intent) { return null; }
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 }
